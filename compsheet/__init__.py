3bb6ee9d662f0553907cea86c687678189af856e
Derek Fujimoto <fujimoto@phas.ubc.ca>
2018-11-29 16:28:10 -0800

    version 1.0.6

--- compsheet/__init__.py
+++ compsheet/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ['multifile_comparer','comparer']
-__version__ = '1.0.5'
+__version__ = '1.0.6'
 __author__ = 'Derek Fujimoto'
 
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     
 setuptools.setup(
     name="compsheet",
-    version="1.0.5",
+    version="1.0.6",
     author="Derek Fujimoto",
     author_email="fujimoto@phas.ubc.ca",
     description="compare microsoft spreadsheet data to check for plagiarism",
